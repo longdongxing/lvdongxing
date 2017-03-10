3b1e8cf0c1c0839a8051ef7474da6095df99331a
吕东星 <15831685489@163.com>
2017-03-10 19:31:22 +0800

    初始化项目

---
+++ NetWorkNews/NetWorkNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetWorkNews
+//
+//  Created by 吕东星 on 17/3/10.
+//  Copyright © 2017年 lddongxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
